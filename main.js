fad90d560554daee138fab1815c9e25b452da510
Alex <alex@alexseifert.com>
2018-07-30 00:50:26 +0200

    Created a custom titlebar for macOS

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: 'hiddenInset'});
 
     // and load the index.html of the app.
     mainWindow.loadFile('index.html');
