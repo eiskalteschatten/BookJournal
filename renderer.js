9299d3234ba8b34aa6f881a7e4ceb00d9ce90b27
Alex <alex@alexseifert.com>
2018-07-30 01:01:10 +0200

    Hide the custom titlebar on all non-macOS platforms

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const jquery = require('jquery');
+const os = require('os');
+
+if (os.type() !== 'Darwin') {
+    jquery('.title-bar').hide();
+}
