0641b1cc5f58793913f8bc01abd91c0c24efda43
Alex <alex@alexseifert.com>
2018-07-30 16:02:33 +0200

    Change the default window size

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: 'hiddenInset'});
+    mainWindow = new BrowserWindow({width: 1200, height: 800, titleBarStyle: 'hiddenInset'});
 
     // and load the index.html of the app.
     mainWindow.loadFile('index.html');
