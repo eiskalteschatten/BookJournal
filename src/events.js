687d318979fdc51e44d4fe2dfc3d946ec2c9f426
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2018-08-14 15:30:26 +0200

    Maximize the main window when double clicking the titlebar on macOS

--- src/events.js
+++ src/events.js
@@ -8,3 +8,9 @@ const SidebarListElement = require('./elements/listElement/sidebar');
 $(document).on('click', '.js-sidebar-list-element', function() { // eslint-disable-line
     SidebarListElement.onClick($(this));
 });
+
+if (process.platform === 'darwin') {
+    $('.js-title-bar').dblclick(() => {
+        require('electron').remote.getCurrentWindow().maximize();
+    });
+}
