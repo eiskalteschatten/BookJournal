df288752641ae3e18fa7262606a8f43bc99ed6aa
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2018-08-14 16:45:38 +0200

    Add Category model

---
+++ src/models/category.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+
+const Category = db.define('category', {
+    name: Sequelize.STRING,
+    color: Sequelize.STRING
+});
+
+Category.sync({alter: true});
+
+module.exports = Category;
