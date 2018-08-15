942aad3aaea4963374124d3d610db47fd554c12c
Alex <alex@alexseifert.com>
2018-08-15 13:57:19 +0200

    "Speech" in the input context menu should only show up on macOS

--- src/config/menus/inputCm.js
+++ src/config/menus/inputCm.js
@@ -4,15 +4,20 @@
 const template = [
     {role: 'cut'},
     {role: 'copy'},
-    {role: 'paste'},
-    {type: 'separator'},
-    {
-        label: 'Speech',
-        submenu: [
-            {role: 'startspeaking'},
-            {role: 'stopspeaking'}
-        ]
-    }
+    {role: 'paste'}
 ];
+
+if (process.platform === 'darwin') {
+    template.push(
+        {type: 'separator'},
+        {
+            label: 'Speech',
+            submenu: [
+                {role: 'startspeaking'},
+                {role: 'stopspeaking'}
+            ]
+        }
+    );
+}
 
 module.exports = template;
