0eb9b9a7e0ab312d1064f0f29aa292083bc9cfba
Alex <alex@alexseifert.com>
2018-08-19 00:19:18 +0200

    Add more details to the delete category confirmation dialog

--- src/config/menus/categoryListElementCm.js
+++ src/config/menus/categoryListElementCm.js
@@ -15,8 +15,9 @@ const template = [
         click: async (item, focusedWindow) => {
             dialog.showMessageBox({
                 message: 'Are you sure you want to delete this category?',
+                detail: 'You can\'t undo this action.',
                 buttons: ['No', 'Yes'],
-                type: 'question',
+                type: 'warning',
                 defaultId: 0,
                 cancelId: 0
             }, response => {
