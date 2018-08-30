74270eb6f4188d901938921a7850b1d2b8a66f81
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2018-08-30 10:21:07 +0200

    Preparations for talking to Amazon to retrieve book data

--- src/config/config.js
+++ src/config/config.js
@@ -21,5 +21,25 @@ module.exports = {
     bookcovers: {
         path: path.join(storagePath, 'bookcovers'),
         extensions: ['jpg', 'jpeg', 'png', 'gif', 'svg']
+    },
+    bookInfo: {
+        amazon: {
+            url: 'http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=AKIAI5NHN2ZXOQQRDO4Q&AssociateTag=[Associate ID]&Operation=ItemLookup&ItemId=${isbn}&Timestamp=${timeStamp}&Signature=[Request Signature]'
+        }
     }
 };
+
+
+// https://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemLookup.html
+// http://webservices.amazon.com/onca/xml?
+// Service=AWSECommerceService&
+// AWSAccessKeyId=[AWS Access Key ID]&
+// AssociateTag=[Associate ID]&
+// Operation=ItemLookup&
+// ItemId=0316067938&
+// ResponseGroup=Reviews&
+// TruncateReviewsAt="256"&
+// IncludeReviewsSummary="False"&
+// Version=2013-08-01
+// &Timestamp=[YYYY-MM-DDThh:mm:ssZ]
+// &Signature=[Request Signature]
