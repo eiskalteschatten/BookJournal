f7ba0378f56b2a4f8c6cbcc59dd26d05a5159f0a
Alex <alex@alexseifert.com>
2018-09-20 21:43:17 +0200

    Setup statistics rendering events

--- src/events/statistics.js
+++ src/events/statistics.js
@@ -3,6 +3,14 @@
 const {ipcRenderer} = require('electron');
 
 
-ipcRenderer.on('statistics-years-months', (event, allDatesRead) => {
+ipcRenderer.on('statistics-set-session-storage', (event, allDatesRead) => {
     sessionStorage.setItem('allDatesRead', JSON.stringify(allDatesRead));
+});
+
+ipcRenderer.on('statistics-render-page-book-count-year', (event, countsYearNumber) => {
+    console.log('countsYearNumber', countsYearNumber);
+});
+
+ipcRenderer.on('statistics-render-page-book-count-month-year', (event, countsMonthYearNumber) => {
+    console.log('countsMonthYearNumber', countsMonthYearNumber);
 });
