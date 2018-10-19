99bdad08e30c920a2a993f1541ffdd84bd2e1197
Alex <alex@alexseifert.com>
2018-10-20 00:18:57 +0200

    Optimize the SCSS build process

---
+++ build-scripts/build.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const path = require('path');
+const compileSass = require('compile-sass');
+const fs = require('fs');
+
+const pathToCss = path.resolve('./src/assets/css');
+const pathToScss = path.resolve('./src/assets/scss');
+
+
+async function build() {
+    try {
+        fs.readdir(pathToScss, async (error, files) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            const scssFiles = [];
+
+            for (const file of files) {
+                if (file.charAt(0) !== '_' && path.extname(file) === '.scss')
+                    scssFiles.push(file);
+            }
+
+            await compileSass.compileSassAndSaveMultiple({
+                sassPath: pathToScss,
+                cssPath: pathToCss,
+                files: scssFiles
+            });
+        });
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
+build();
