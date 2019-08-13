510582cbe375b26cc671c7c51567c8732b1e2630
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-08-13 11:57:00 +0200

    The selection bar now saves in the book form

--- src/events/forms.js
+++ src/events/forms.js
@@ -3,8 +3,8 @@
 const $ = require('jquery');
 
 $(document).on('click', '.js-selector', function() {
-    const $inputField = $(this).closest('.js-selector-input');
-    $inputField.val($(this).data('value'));
+    const $inputField = $(this).parents('.js-selection-bar').find('.js-selector-input');
+    $inputField.val($(this).data('value')).trigger('change');
     $(this).siblings('.selected').removeClass('selected');
     $(this).addClass('selected');
 });
