27b63443c22868606b00837cd1a00308f128f52f
Alex <alex@alexseifert.com>
2020-05-21 22:00:48 +0200

    Fix linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { IntlProviderWrapper } from './intl/IntlContext';
 
 const App: React.FC = () => {
   // TODO: allow the saved locale from the DB to override the system's settings
-  const locale = navigator.language.split('-')[0] || 'en';
+  const locale = navigator.language.split('-')[0] || 'en';  // eslint-disable-line no-undef
 
   return (
     <IntlProviderWrapper injectedLocale={locale}>
