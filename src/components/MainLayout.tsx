064399f446f64213a1ab5ff9b946c4e3f9f68852
Alex <alex@alexseifert.com>
2020-05-21 22:39:20 +0200

    Add MainLayout

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainLayout: React.FC = () => {
+
+  return (
+    <div>
+      test
+    </div>
+  );
+};
+
+export default MainLayout;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/light.scss';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
