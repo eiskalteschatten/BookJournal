4ca5e6fde25f2dd1f7df34d530c5553dcadf33d6
Alex <alex@alexseifert.com>
2020-05-21 20:21:37 +0200

    Add more options to the main window

--- src/main/Main.ts
+++ src/main/Main.ts
@@ -1,6 +1,7 @@
 import { BrowserWindow, BrowserWindowConstructorOptions } from 'electron';
 import path from 'path';
 
+import config from '../config';
 
 export default class Main {
   static mainWindow: Electron.BrowserWindow | undefined;
@@ -13,16 +14,35 @@ export default class Main {
     }
   }
 
-  private static onClose(): void {
-    // Dereference the window object.
-    Main.mainWindow = undefined;
+  private static async onClose(): Promise<void> {
+    try {
+      if (Main.mainWindow) {
+        const windowBounds = Main.mainWindow.getBounds();
+
+        const values = {
+          windowWidth: windowBounds.width,
+          windowHeight: windowBounds.height,
+          windowX: windowBounds.x,
+          windowY: windowBounds.y,
+          windowIsFullScreen: Main.mainWindow.isFullScreen(),
+          windowIsMaximized: Main.mainWindow.isMaximized(),
+        };
+
+        // await preferences.update(values);
+
+        Main.mainWindow = undefined;
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
 
   private static onReady(): void {
-    if (process.env.NODE_ENV === 'development') {
+    // if (process.env.NODE_ENV === 'development') {
       // Open the DevTools.
       // Main.BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-    }
+    // }
 
     const browserWindowOptions: BrowserWindowConstructorOptions = {
       width: 800,//preferences.windowWidth,
@@ -45,6 +65,12 @@ export default class Main {
     Main.mainWindow = new Main.BrowserWindow(browserWindowOptions);
 
     if (Main.mainWindow) {
+      // if (preferences.windowIsMaximized) {
+      //   Main.mainWindow.maximize();
+      // }
+
+      // Main.mainWindow.setFullScreen(preferences.windowIsFullScreen || false);
+
       Main.mainWindow.loadURL(
         process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000'
@@ -64,5 +90,6 @@ export default class Main {
     Main.application = app;
     Main.application.on('window-all-closed', Main.onWindowAllClosed);
     Main.application.on('ready', Main.onReady);
+    Main.application.setName(config.app.name);
   }
 }
