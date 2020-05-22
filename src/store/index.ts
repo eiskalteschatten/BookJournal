98667a706362ade40df6ecfbb3d6786d656991dd
Alex Seifert <alex@alexseifert.com>
2020-05-22 12:10:45 +0200

    Fix a build problem

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import preferences, { PreferencesState } from './reducers/preferencesReducer';
 
 const devExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;  // eslint-disable-line no-undef
 
-const composeEnhancers = devExtension && process.env.NODE_ENV !== 'production' ? devExtension : compose;
+const composeEnhancers = devExtension && process.env.NODE_ENV === 'development' ? devExtension : compose;
 
 const reducer = combineReducers({
   preferences
